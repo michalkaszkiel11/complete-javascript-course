b0dbfd7b2f8d3acb1630d7c1ecc4d3b5760ae92f
Michal <120386627+michalkaszkiel@users.noreply.github.com>
2023-06-06 14:00:28 +0200

    Update script.js

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -3,4 +3,27 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-///////////////////////////////////////
+const countryPicker = function (countryName) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${countryName}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText); // destructuring an array with an object wth [].
+    console.log(data);
+    const html = `
+<article class="country">
+<img class="country__img" src="${data.flags.png}" />
+<div class="country__data">
+  <h3 class="country__name">${data.name.common}</h3>
+  <h4 class="country__region">${data.region}</h4>
+  <p class="country__row"><span>👫</span>${data.population}people</p>
+  <p class="country__row"><span>🗣️</span>${data.languages.pol}</p>
+  <p class="country__row"><span>💰</span>${data.currencies.PLN.name}</p>
+</div>
+</article>`;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+countryPicker('poland');
