547c583f544aef7680d90d2bc7b9674d86752886
Michal <120386627+michalkaszkiel@users.noreply.github.com>
2023-06-08 21:01:03 +0200

    Update script.js

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -3,7 +3,23 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-const countryPicker = function (countryName) {
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${data.population}</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0]}</p>
+    <p class="country__row"><span>💰</span>${data.currencies.name}</p>
+  </div>
+  </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const countryNneighbourPicker = function (countryName) {
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v3.1/name/${countryName}`);
   request.send();
@@ -11,19 +27,21 @@ const countryPicker = function (countryName) {
   request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText); // destructuring an array with an object wth [].
     console.log(data);
-    const html = `
-<article class="country">
-<img class="country__img" src="${data.flags.png}" />
-<div class="country__data">
-  <h3 class="country__name">${data.name.common}</h3>
-  <h4 class="country__region">${data.region}</h4>
-  <p class="country__row"><span>👫</span>${data.population}people</p>
-  <p class="country__row"><span>🗣️</span>${data.languages.pol}</p>
-  <p class="country__row"><span>💰</span>${data.currencies.PLN.name}</p>
-</div>
-</article>`;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+    renderCountry(data);
+
+    //neighbour picker
+    const neighbours = data.borders[0];
+    if (!neighbours) {
+      return;
+    }
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbours}`);
+    request2.send();
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
-countryPicker('poland');
+countryNneighbourPicker('poland');
